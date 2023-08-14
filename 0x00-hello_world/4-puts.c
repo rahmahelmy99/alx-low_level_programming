646aacafd8470ae76f146e1c029bd7fd054f03e2
Rahma <helmyrahma4@gmail.com>
2023-08-14 16:20:31 -0700

    4-puts.c file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(){
+/*
+ * main:using puts function to print a sentence
+ * description:puts function used to print a string following by a newline 
+ * */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
