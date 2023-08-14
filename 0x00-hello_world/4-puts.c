9fa9da042d762fa7867617dbfde1a2ac7a19b783
Rahma <helmyrahma4@gmail.com>
2023-08-14 16:40:56 -0700

    4-puts.cfile added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main:using puts function to print a sentence
- * description:puts function used to print a string following by a newline 
- * */
+/**
+ * main - Entry point
+ * Description:the 'puts' function used to print a string following by a newline
+ * Return:the return value is 0 means succes
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
