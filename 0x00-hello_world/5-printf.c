fe81ec865bb6746c17167488578cbc8c5ec3da70
Rahma <helmyrahma4@gmail.com>
2023-08-14 16:48:02 -0700

    5-printf.c file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ *main -Entry point
+ *
+ *Description:using 'printf' function print string with newline
+ *
+ *Return: the return value is 0 means success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
