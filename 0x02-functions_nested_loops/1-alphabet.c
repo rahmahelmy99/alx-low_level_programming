443e1b556651062a2e43fe717f5a493b5786c970
Rahma <helmyrahma4@gmail.com>
2023-08-19 05:31:48 -0700

    1-alphabet.c file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prototype function that prints
+ *the alphabet in lowercase.
+ *
+ * return: return 0 if success.
+ *
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c < 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
