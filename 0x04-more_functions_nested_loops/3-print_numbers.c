f16c63afc7b3066580c75458135e2842e6e492ec
Rahma <helmyrahma4@gmail.com>
2023-08-18 19:11:47 -0700

    3-print_numbers.c file added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - prototype function that
@@ -16,5 +15,4 @@ void print_numbers(void)
 		_putchar(i + 48);
 	}
 	_putchar('\n');
-	return (0);
 }
