2a288c2efd5be0068e8b6597442e10c67b07c710
Rahma <helmyrahma4@gmail.com>
2023-08-18 19:08:29 -0700

    3-print_numbers.c file added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prototype function that
+ *prints the numbers, from 0 to 9.
+ *
+ * Return: 0 if program success.
+ *
+*/
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9 ; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+	return (0);
+}
