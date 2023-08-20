b95e05dd34d49835b9ff6553a8590970a7b799e7
Rahma <helmyrahma4@gmail.com>
2023-08-19 22:15:07 -0700

    printf("%d\n", sum);

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: program that prints alphabet in lowercase.
+ *
+ * Return: return value is 0 means success.
+ *
+*/
+
+int main(void)
+{
+	int i, sum;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+			printf("%d\n", sum);
+		}
+	}
+	return (0);
+}
