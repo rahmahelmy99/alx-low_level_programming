d1aa5f51812b6e39c08fc1b966c1cea6f2b7b067
Rahma <helmyrahma4@gmail.com>
2023-08-20 00:21:41 -0700

    103-fibonacci.c file added

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: program that prints alphabet in lowercase.
+ *
+ * Return: return value is 0 means success.
+ *
+*/
+
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, sum;
+	int even;
+
+	while (1)
+	{
+		sum = f1 + f2;
+		if (sum < 4000000)
+			break;
+		if ((sum % 2 == 0))
+		{
+			even += sum;
+		}
+		f1 = f2;
+		f2 = sum;
+	}
+	printf("%i\n", even);
+	return (0);
+}
