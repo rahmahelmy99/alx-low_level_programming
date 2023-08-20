25ae0e017df7a9b327a38e866b821d803ef2a7dc
Rahma <helmyrahma4@gmail.com>
2023-08-20 00:34:43 -0700

    104-fibonacci.c file added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: program that prints fibonachi numbers
+ *
+ * Return: return value is 0 means success.
+ *
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = f1 + f2;
+		printf("%ld", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (i == 98)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
