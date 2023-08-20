cc6594980606f6406cbab58b6009ad79e2a52255
Rahma <helmyrahma4@gmail.com>
2023-08-19 20:11:16 -0700

    10-add.c file added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prototype function that adds two
+ * integers and returns the result.
+ *
+ * @a: input first number.
+ * @b: input second number.
+ *
+ * Return: the result of function.
+ *
+*/
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
