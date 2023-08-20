2f7c48bef4a63efc0cc0e7badff2015dfcd3a5fd
Rahma <helmyrahma4@gmail.com>
2023-08-19 21:44:40 -0700

    100-times_table.c file added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar(48);
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
