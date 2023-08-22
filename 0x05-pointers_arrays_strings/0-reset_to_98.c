510317e89494ecfe5038622b13b32a2896d41965
Rahma <helmyrahma4@gmail.com>
2023-08-22 00:30:06 -0700

    0-reset_to_98.c file added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - prototype function that prints
+ *@*n: a pointer.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+tasks of topics (pointers &arrays and strings)
