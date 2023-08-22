4b56453425d52ada9e725fe040c45396583da4f2
Rahma <helmyrahma4@gmail.com>
2023-08-22 02:49:28 -0700

    3-puts.c file added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prototype function that prints a string.
+ *
+ *@str: input that ponter takes.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+void _puts(char *str)
+{
+	int count;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
