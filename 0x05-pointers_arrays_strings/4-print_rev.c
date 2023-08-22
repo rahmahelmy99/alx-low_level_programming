f21b46556bf6f01bd5f69b72dffb6cb418094ea1
Rahma <helmyrahma4@gmail.com>
2023-08-22 04:01:57 -0700

    4-print_rev.c file added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prototype function that prints
+ * a string, in reverse.
+ *
+ *@s: input that pointer take.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (--i)
+		_putchar(s[i]);
+	putchar('\n');
+}
