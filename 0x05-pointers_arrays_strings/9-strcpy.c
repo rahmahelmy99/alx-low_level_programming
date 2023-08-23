688c7d7c2dbcf81fb8896d1710ea32886864209c
Rahma <helmyrahma4@gmail.com>
2023-08-23 06:33:51 -0700

    9-strcpy.c file added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - prototype function that copies the string
+ * pointed to by src,including null byte (\0).
+ *
+ *@dest: first input array.
+ *@src: second inpuy array.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] == '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
