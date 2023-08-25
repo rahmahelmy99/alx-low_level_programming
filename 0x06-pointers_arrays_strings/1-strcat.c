4685335770dea72709c66a7b80b54039d66875f4
Rahma <helmyrahma4@gmail.com>
2023-08-25 14:10:51 -0700

    1-strcat.c file added

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - prototype function that concatenates
+ * two strings.
+ *
+ *@dest: first input array.
+ *@src: second input array.
+ *@n: input number.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int ln, i;
+
+	for (ln = 0; dest[ln]; ln++)
+		;
+	for (i = 0; i < n; i++)
+		dest[ln++] = src[i];
+	return (dest);
+}
