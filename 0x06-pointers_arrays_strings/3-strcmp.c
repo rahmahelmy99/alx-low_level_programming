66963823fae76a87c0ec2f988aeecda30bd9e686
Rahma <helmyrahma4@gmail.com>
2023-08-25 18:10:23 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,17 +13,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i;
 	int res = 0;
 
-	while (*s1)
+	for (i = 0; s1[i]; i++)
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			res = ((int)*s1 - 48) - ((int)*s2 - 48);
+			res = ((int)s1[i] - 48) - ((int)s2[i] - 48);
 			break;
 		}
-		s1++;
-		s2++;
 	}
 	return (res);
 }
