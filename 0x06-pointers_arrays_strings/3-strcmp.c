4acee9495254ff864848ec4291f9d63eef73b405
Rahma <helmyrahma4@gmail.com>
2023-08-25 17:45:57 -0700

    3-strcmp.c file added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - prototype function that compares two strings.
+ *
+ *@s1: first input character.
+ *@s2: second input character.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int res = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+			res = ((int)s1[i] - 48) - ((int)s2[i] - 48);
+		else
+			res = 0;
+		i++;
+	}
+	return (res);
+}
