d60d79d90228a44a4050d8c15e974cb31b3e4453
Rahma <helmyrahma4@gmail.com>
2023-08-25 17:04:01 -0700

    2-strncpy.c file added

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int ln, i;
+	int i;
 
-	for (ln = 0; src[ln]; ln++)
-		;
-	for (i = 0; i <= ln; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
