704962b0cb4d44865902c1e0d619d9f3800ae2af
Rahma <helmyrahma4@gmail.com>
2023-08-28 09:33:02 -0700

    readme file added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - prototype function that fills memory
+ *with a constant byte.
+ *
+ *@s: input array.
+ *@b: input character.
+ *@n: input number.
+ *
+ * Return: 0 if success.
+ *
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+tasks of pointer to pointer and 2D arrays
