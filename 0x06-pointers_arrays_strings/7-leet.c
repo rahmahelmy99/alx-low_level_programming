a64eb71d3777ba3d46f50ce5b277de71a153f5ed
Rahma <helmyrahma4@gmail.com>
2023-08-28 05:47:13 -0700

    7-leet.c file update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,13 +13,13 @@
 
 char *leet(char *s)
 {
-	int i = 0, j;
+	int i, j;
 	char c[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int n[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; i < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == c[j])
 			{
