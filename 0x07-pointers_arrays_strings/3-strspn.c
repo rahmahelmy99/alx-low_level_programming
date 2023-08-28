cd3d44b83a24abb70ed450a6077cef2aebc20585
Rahma <helmyrahma4@gmail.com>
2023-08-28 16:35:31 -0700

    3-strspn.c file added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - prototype function that gets the length
+ * of a prefix substring.
+ *
+ *@s: input array.
+ *@accept: input array to check.
+ *
+ * Return: number of bytes of the accept.
+ *
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[i])
+				continue;
+			else if (accept[j + 1] == '\0')
+				break;
+		}
+	}
+	return (i);
+}
