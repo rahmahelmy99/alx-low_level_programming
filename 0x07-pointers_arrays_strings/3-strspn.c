588d5c0404d10c738d4e2778fc24c489802988df
Rahma <helmyrahma4@gmail.com>
2023-08-28 16:39:48 -0700

    3-strspn.c file added

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -20,10 +20,10 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[i])
+			if (s[i] == accept[j])
 				break;
 			else if (accept[j + 1] == '\0')
-				return (i)
+				return (j);
 		}
 	}
 }
