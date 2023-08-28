b8423b6627f2cf295b25f3506466c68345678518
Rahma <helmyrahma4@gmail.com>
2023-08-28 14:38:57 -0700

    0-memset.c file added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,10 +17,13 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
-		n--;
+		if (n > 0)
+		{
+			s[i] = b;
+			n--;
+		}
 	}
 
 	return (s);
