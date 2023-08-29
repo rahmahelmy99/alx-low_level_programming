afe04ea64ba5b7d57a57cc098e661f66f2ab2a22
Rahma <helmyrahma4@gmail.com>
2023-08-28 18:56:45 -0700

    5-strstr.c file added

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,10 +19,7 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] == haystack[i] && needle[j] != '\0'; j++)
-		{
-			needle++;
-			haystack++;
-		}
+			;
 		if (needle[j] == '\0')
 			return (haystack);
 	}
