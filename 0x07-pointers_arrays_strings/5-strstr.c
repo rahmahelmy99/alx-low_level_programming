2dd84005a8cbde73f3df3854b5498943d4c47a4e
Rahma <helmyrahma4@gmail.com>
2023-08-28 18:37:48 -0700

    5-strstr.c file added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - prototype function that 
+ *
+ *@haystack: input array.
+ *@needle: input array.
+ *
+ * Return:  a pointer to the beginning of the located
+ * substring, or NULL if the substring is not found.
+ *
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] == hystack[i] && needle[j] != '\0'; j++)
+		{
+			needle++;
+			haystack++;
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack)
+		}
+	}
+	return ('\0');
+}
