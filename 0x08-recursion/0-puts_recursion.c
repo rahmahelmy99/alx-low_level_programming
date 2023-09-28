ed928550b6618c97e69df9a424d4de8fec49796f
rahma <helmyrahma4@gmail.com>
2023-09-28 03:24:32 -0700

    added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,7 +12,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return ('\n');
-	_putchar(*s);
-	_puts_recursion(s+1);
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
 }
