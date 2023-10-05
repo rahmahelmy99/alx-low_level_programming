9417f47dcf2d2af5b602f295e4ecbb91a5c751bb
rahma <helmyrahma4@gmail.com>
2023-10-04 18:09:08 -0700

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -25,7 +25,7 @@ char *_strdup(char *str)
 
 	for (size = 0; str[size] != '\0'; size++)
 		;
-	arr = malloc(size * sizeof(*str));
+	arr = malloc(size * sizeof(*str) + 1);
 
 	if (arr == 0)
 		return (0);
