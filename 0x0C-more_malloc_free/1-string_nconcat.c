7881eca260e2b7cef68bc476d976abe7355278c0
rahma <helmyrahma4@gmail.com>
2023-10-09 16:12:36 -0700

    added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * two strings.
  * @s1: first string.
  * @s2: second string.
- * @:n: input number.
+ * @n: input number.
  * Return: a pointer.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
