9daa3ac0ce4ca4d96727d1d8726c6b07adf64ed6
rahma <helmyrahma4@gmail.com>
2023-10-09 02:57:59 -0700

    added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked -  prototype function that allocates memory
+ * using malloc.
+ * @b: input unsighend number.
+ * Return: a pointer to allocate memory.
+*/
+void *malloc_checked(unsigned int b)
+{
+	
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+readme file of more malloc projects
