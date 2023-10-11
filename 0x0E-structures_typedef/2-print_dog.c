e265a102b1dc4cfa6bbd48a35026bff9ae943829
rahma <helmyrahma4@gmail.com>
2023-10-10 18:45:12 -0700

    added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,11 +5,13 @@
  * print_dog - prototype function that prints a
  * struct dog.
  * @d: a struct copy.
- * return: nothing.
+ * 
+ * Return: nothing.
 */
+
 void print_dog(struct dog *d)
 {
-	if (d != 0)
+	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
