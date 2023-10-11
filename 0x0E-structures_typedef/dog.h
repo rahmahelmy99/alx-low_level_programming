72bf3ba20f8d81fb15ee2e1bcb50a4a9d251f0a3
rahma <helmyrahma4@gmail.com>
2023-10-10 18:57:07 -0700

    added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,12 @@ struct dog
 	char *owner;
 };
 typedof struct dog dog_t;
-
+typedof struct dog_t
+{
+        char *name;
+        float age;
+        char *owner;
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
