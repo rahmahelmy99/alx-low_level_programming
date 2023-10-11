f52300ad06d0513eb1611f2ca096e2667659df46
rahma <helmyrahma4@gmail.com>
2023-10-10 19:00:04 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,13 +12,8 @@ struct dog
 	float age;
 	char *owner;
 };
-typedof struct dog dog_t;
-typedof struct dog_t
-{
-        char *name;
-        float age;
-        char *owner;
-}dog_t;
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
