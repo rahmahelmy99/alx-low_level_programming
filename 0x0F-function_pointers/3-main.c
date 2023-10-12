6b4897fa598b239ca14645f0ae57b7a88b708485
rahma <helmyrahma4@gmail.com>
2023-10-11 19:04:48 -0700

    added

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,5 +8,28 @@
 */
 int main(argc, **argv)
 {
-	
+	int (*fun_op)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Eror\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	func_op = get_op_func(argv[2]);
+	if (!func_op)
+	{
+		printf("Eror\n");
+		exit(99);
+	}
+	if (!b && argv[2][0] == '/' || argv[2][0] == '%')
+        {
+		printf("Eror\n");
+		exit(100);
+	}
+	printf("%d\n" func_op(a, b));
+	return(0);
 }
