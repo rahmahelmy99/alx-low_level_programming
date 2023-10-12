ad3c65e3847e3ca215c73990be6df7b0920ae5cc
rahma <helmyrahma4@gmail.com>
2023-10-11 19:29:58 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,10 +16,10 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i;
 
-	while(i < 5)
+	while (i < 5)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
-			return(ops[i].f);
+			return (ops[i].f);
 		i++;
 	}
 	return (0);
