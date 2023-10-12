5bee008fbb7512794d600ab027c61803dcb009a2
rahma <helmyrahma4@gmail.com>
2023-10-11 17:39:24 -0700

    added

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,19 +7,20 @@
  * @cmp: a pointer to a function.
  * return: returns the index of the first element for which
  * the cmp function does not return 0.
- * */
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int *i =  array + size;
-	if (array && size && cmp)
+	int i = 0;
+
+	if (array && size > 0 && cmp)
 	{
-		while (array < i)
+		for (; i < size ; i++)
 		{
-			if (cmp(*array))
-			{
-				return (i)
-				array++;
-			}
+			if (cmp(array[i]))
+				return (i);
+			else
+				return (-1);
 		}
 	}
+	return (-1);
 }
