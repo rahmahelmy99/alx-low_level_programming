510d3fbe72b7729d7c0d5faecfc9e8ff4882d38b
rahma <helmyrahma4@gmail.com>
2023-10-19 16:47:15 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h[i] != '\0'; i++)
+	for (i = 0; h->str[i] != '\0'; i++)
 	{
 		if (h->str == 0)
 			printf("[%d] (nil)", _strlen(h->str));
