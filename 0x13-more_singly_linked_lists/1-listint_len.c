7f576deb0a04f005f8630a6439ca1eacf76346a7
rahma <helmyrahma4@gmail.com>
2023-10-24 17:51:10 -0700

    added

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prototype function that prints the number of elements
+ * in a listint_t list.
+ * @h: head of the linked list.
+ * Return: number of nodes.
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
