4d217cff1284103a12d2d3665b9a4bbf30763cfa
rahma <helmyrahma4@gmail.com>
2023-10-24 19:03:09 -0700

    added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - prototype function that freess a listint_t list.
+ * @head: head node.
+ * Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *t;
+
+	if (!head)
+		return;
+	node = *head;
+	while (head)
+	{
+		t = node;
+		node = node->next;
+		free(t);
+	}
+	*head = 0;
+}
