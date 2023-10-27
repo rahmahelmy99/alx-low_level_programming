c0067211f0df9dce278c515e774961afaac327f3
rahma <helmyrahma4@gmail.com>
2023-10-26 20:04:52 -0700

    added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,14 @@ void print_binary(unsigned long int n)
 	int digit = sizeof(n) * 8;
 	int bnum = 0;
 
-	while(digit)
+	while (digit)
 	{
 		if (n & 1L << --digit)
 		{
 			_putchar('1');
 			bnum++;
 		}
-		else if (bnum == 0)
+		else if (bnum)
 			_putchar('0');
 	}
 	if (!bnum)
