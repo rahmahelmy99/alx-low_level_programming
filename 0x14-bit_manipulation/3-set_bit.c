11eb73c5c15785af1a50eb35c13356edc7047846
rahma <helmyrahma4@gmail.com>
2023-10-29 03:26:21 -0700

    added

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	*n |= 1L << index;
-	if (*n >> index & 1L == 1)
+	if (*n >> index & 1 == 1)
 		return (1);
 	else
 		return (0);
