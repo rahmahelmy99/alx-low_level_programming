6dd27ede3b746b97cf65607249b7ea5e4cc3b8bc
rahma <helmyrahma4@gmail.com>
2023-11-03 19:55:30 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,5 @@ int create_file(const char *filename, char *text_content)
 		byt = write(fdesc, text_content, len);
 	close(fdesc);
 
-	if (byt == len)
-		return (1);
-	else
-		return (-1);
+	return (byt == len ? 1 : -1);
 }
