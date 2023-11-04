7e4922108ab5f61c8261710c01d241a5cd5d90af
rahma <helmyrahma4@gmail.com>
2023-11-03 19:47:37 -0700

    added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	if (!filename)
 		return (-1);
-	fdesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fdesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fdesc == -1)
 		return (-1);
 	if (len)
