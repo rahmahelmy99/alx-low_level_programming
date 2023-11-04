ebe725283344df39fd898639e602f4016af22d0e
rahma <helmyrahma4@gmail.com>
2023-11-03 19:39:01 -0700

    added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t byt = 0;
 	ssize_t len;
 
-	for (len = 0; text_content[len]; len++)
+	for (len = 0; *text_content; len++)
 		;
 	if (!filename)
 		return (0);
