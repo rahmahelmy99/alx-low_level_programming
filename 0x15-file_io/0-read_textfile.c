2d97d4ebfe2ae896b83c42a81ba616c734515a34
rahma <helmyrahma4@gmail.com>
2023-11-27 16:03:52 -0800

    added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - prototype function that reads a text file and prints it
+ * to the POSIX standard output.
+ * filename: it is a name of the file.
+ * letters: it is a number of letters it should read and print.
+ * Return: an actual number of letters it could read and print.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fdesc;
+	ssize_t byt;
+	char buf[1024 * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fdesc = open(filename, O_RDONLY);
+	if (fdesc == -1)
+		return (0);
+	byt = read(fdesc, &buf[0], letters);
+	byt = write(STDOUT_FILENO, &buf[0], byt);
+	close(fdesc);
+	return (byt);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+readme file of file handling project
