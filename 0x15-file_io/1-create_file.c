8a499e8b869e514ad5c169334b2dd8a12ae3cfc3
rahma <helmyrahma4@gmail.com>
2023-11-27 16:07:21 -0800

    added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - that creates a file.
+ * @filename: filename.
+ * @text_content: content of file.
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fdesc;
+	ssize_t byt = 0;
+	ssize_t len;
+
+	for (len = 0; text_content[len] != 0; len++)
+		;
+	if (!filename || !text_content)
+		return (-1);
+	fdesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fdesc == -1)
+		return (-1);
+	if (len)
+		byt = write(fdesc, text_content, len);
+	close(fdesc);
+
+	return (byt == len ? 1 : -1);
+}
